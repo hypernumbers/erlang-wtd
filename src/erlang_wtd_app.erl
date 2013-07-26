65820ac1f916414f0fee12de086e3fc9dfa7ecc8
Gordon Guthrie <gordon@vixo.com>
2013-07-26 16:21:02 +0000

    added cowboy as a dependency and added a start script

--- src/erlang_wtd_app.erl
+++ src/erlang_wtd_app.erl
@@ -3,13 +3,20 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([
+         start/0, start/2,
+         stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
+start() -> start([], []).
+
 start(_StartType, _StartArgs) ->
+    ok = application:start(crypto),
+    ok = application:start(ranch),
+    ok = application:start(cowboy),
     erlang_wtd_sup:start_link().
 
 stop(_State) ->
---
+++ start_wtd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+erl -smp -pa ./ebin -pa ./deps/*/ebin -s erlang_wtd_app
